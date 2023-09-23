520d4d083078e31955d672c4999251318da5a341
wjdigital <45724694+wjdigital@users.noreply.github.com>
2023-09-23 18:09:38 -0300

    Update entrypoint.sh

--- viewer/entrypoint.sh
+++ viewer/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEXT_PUBLIC_VIEWER_URL="https://viewer.autobet.one/"
+NEXT_PUBLIC_VIEWER_URL="https://viewer.autobet.one"
 printenv > ./apps/viewer/.env.production
 cat ./apps/viewer/.env.production
 
